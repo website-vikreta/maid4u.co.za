4f65215eb4e110a6cbde2a1c0fd9e619116b5fa0
sbgprojects <shubham.gujarathi@decartem.com>
2021-02-11 11:09:12 +0530

    Build One

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    // navigation toggle
+    $("nav .toggle").click(function () {
+        $(this).toggleClass('active');
+        $('nav .nav-tab').toggleClass('active');
+    })
+
+
+    $('.gallery .gallery-wrapper').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+})
